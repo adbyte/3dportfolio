2619e76c630895a6eaf155d5ea77e5c6831c0a88
Aditi Jain <jainaditi1401@gmail.com>
2025-06-13 16:18:42 +0530

    Hero and Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { logo, close, menu } from '../assets';
 
 const Navbar = () => {
 
-  const [active, setActiive] = useState("");
+  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
 
 
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className = 'w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to = '/' className="flex items-center gap-2"
         onClick = {() => {
-          setActiive("");
+          setActive("");
           window.scrollTo(0, 0);
         }
       }>
@@ -33,7 +33,7 @@ const Navbar = () => {
                 `${active === link.title ? "text-white" : "text-secondary"}
                 hover:text-white text-[18px] font-medium cursor-pointer`
               }
-              onClick = {() => setActiive(link.title)}>
+              onClick = {() => setActive(link.title)}>
           
               <a href={`#${link.id}`}> {link.title} </a>
             </li>
@@ -42,31 +42,33 @@ const Navbar = () => {
 
         </ul>
         <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img src = {toggle? close : menu} alt = "menu" className = "w=[28px] h-[28px] object-contain cursor-pointer"
-          onClick = {() => setToggle(!toggle)} />
-          
+          <img 
+  src={toggle ? close : menu} 
+  alt="menu" 
+  className="w-[28px] h-[28px] object-contain cursor-pointer"  // Fixed square bracket syntax
+  onClick={() => setToggle(!toggle)} 
+/>
           <div className = {`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
                 <ul className='list-none flex justify-end items-start flex-col gap-4'>
                 {navLinks.map((link) => (
                   <li
-                    key = {link.id}
-                    className = {
-                      `${active === link.title ? "text-white" : "text-secondary"}
-                      hover:text-white text-[18px] font-medium cursor-pointer`
-                    }
-                    onClick = {() => setActiive(link.title)}>
-                
-                    <a href={`#${link.id}`}> {link.title} </a>
+                    key={link.id}
+                    className={
+                    `${active === link.title ? "text-white" : "text-secondary"}
+                    font-poppins font-medium cursor-pointer text-[16px]`}
+                    
+                    onClick={() => {
+                      setActive(link.title);
+                      setToggle(!toggle);
+                    }}>
+
+                    <a href={`#${link.id}`}>{link.title}</a>
                   </li>
-                )
-              )}
-
-              </ul>
+                ))}
+                </ul>
           </div>
-
         </div>
       </div>
-
     </nav>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const ComputersModel = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+   
   return (
-    <div>Computers</div>
-  )
+    <>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={2} />
+      <primitive 
+        object = {computer.scene}
+        scale = {0.75}
+        position = {[0, -3.75, -1.25]} 
+        rotation = {[ -0.01, -0.2, -0.1]}/>
+    </>
+  );
 }
 
-export default Computers
+const Computers = () => {
+  return(
+    <Canvas 
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}> 
+        <OrbitControls 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ComputersModel />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+}
+
+export default Computers;
